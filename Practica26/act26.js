2d7c793d3f90ce5b17aa950b99c1875a92123a3e
NancyPA <nancypadilla166@gmail.com>
2023-11-24 23:50:06 -0800

    Continuamos con js, tipos de operadores. PARTE 1

---
+++ Practica26/act26.js
@@ -0,0 +1,10 @@
+var num1 = 5;
+var num2 = 8;
+
+if (num1 <= num2){
+alert(num1+" no es mayor que "+num2);
+} if(num2 >= 0){
+    alert(num2+" Es positivo");
+}if(++num1 < num2){
+    alert("Incrementar uno en "+num1+", no lo hace mayor que "+num2);
+}
