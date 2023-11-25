e1db62e8850a003d78f890a69e54e26ca189a0be
NancyPA <nancypadilla166@gmail.com>
2023-11-24 23:45:02 -0800

    Comenzamos con js, operaciones basicas

---
+++ Practica24/act24.js
@@ -0,0 +1,35 @@
+/*
+alert("Hola mundo!");
+
+
+var numero1 = 2;
+var numero2 = 3;
+var resultado = numero1  + numero2;
+var mensaje = "El resultado es: ";
+
+alert(mensaje+resultado);
+
+
+var mensaje = "Bienvenido a nuestro sitio web,";
+var texto_1 = " texto comillas dobles,";
+var texto_2 = ' texto comillas simples...';
+
+alert(mensaje+texto_1+texto_2);
+
+
+var array = ['tijuana','rosarito','ensenada','mexicali','tecate'];
+
+alert(array);
+*/
+
+var num1 = parseInt(prompt("Ingrese un numero: "));
+var num2 = parseInt(prompt("Ingrese otro numero: "));
+
+var suma = num1 + num2;
+var resta = num1 - num2;
+var multiplicacion = num1 * num2;
+var division = num1 / num2;
+
+
+alert(num1+" y "+num2+"\nLa suma es: "+ suma + "\nLa resta es: "+ resta+ "\nLa multiplicacion es: "+ 
+multiplicacion+"\nLa division es: "+division+ "\n_____ FIN XD ____")
